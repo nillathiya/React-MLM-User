f21228c6345b3d2a8d1a3a2e9d45d601501321d7
nillathiya <73015219+nillathiya@users.noreply.github.com>
2025-02-19 09:53:40 +0530

    Fix: fix useState undefiend issue from addFund page.

--- src/pages/Fund/addFund.jsx
+++ src/pages/Fund/addFund.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React,{useState} from "react";
 import MasterLayout from "../../masterLayout/MasterLayout";
 import Breadcrumb from "../../components/Breadcrumb";
-import './fund.css'
+import "./fund.css";
 import {
   useAccount,
   useWriteContract,
@@ -36,60 +36,63 @@ const AddFund = () => {
 
   return (
     <>
-        <MasterLayout>
-     <div className="p-4">
-      <h2 className="text-xl font-semibold mb-4">Send USDT</h2>
-      <input
-        type="text"
-        placeholder="Recipient Address"
-        className="border p-2 rounded w-full mb-2"
-        value={recipient}
-        onChange={(e) => setRecipient(e.target.value)}
-      />
-      {/* <input
+      <MasterLayout>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4">Send USDT</h2>
+          <input
+            type="text"
+            placeholder="Recipient Address"
+            className="border p-2 rounded w-full mb-2"
+            value={recipient}
+            onChange={(e) => setRecipient(e.target.value)}
+          />
+          {/* <input
         type="text"
         placeholder="Amount"
         className="border p-2 rounded w-full mb-2"
         value={amount}
         onChange={(e) => setAmount(e.target.value)}
       /> */}
-      <button
-        className="bg-blue-500 text-white p-2 rounded"
-        onClick={() =>
-          writeContractAsync({
-            abi,
-            address: USDT_ADDRESS,
-            functionName: "transfer",
-            args: ["0x3Ca63F13dfDC749Cc5565c4Ab9b00657e1b4f379", amount],
-          })
-        }
-        // disabled={isLoading}
-      >
-        {/* {isLoading ? "Sending..." : "Send USDT"} */}
-        send USDT
-      </button>
-      {hash && <div>Transaction Hash: {hash}</div>}
-      {isConfirming && <div>Waiting for confirmation...</div>}
-      {isConfirmed && <div>Transaction confirmed.</div>}
+          <button
+            className="bg-blue-500 text-white p-2 rounded"
+            onClick={() =>
+              writeContractAsync({
+                abi,
+                address: USDT_ADDRESS,
+                functionName: "transfer",
+                args: ["0x3Ca63F13dfDC749Cc5565c4Ab9b00657e1b4f379", amount],
+              })
+            }
+            // disabled={isLoading}
+          >
+            {/* {isLoading ? "Sending..." : "Send USDT"} */}
+            send USDT
+          </button>
+          {hash && <div>Transaction Hash: {hash}</div>}
+          {isConfirming && <div>Waiting for confirmation...</div>}
+          {isConfirmed && <div>Transaction confirmed.</div>}
 
-      {/* {isSuccess && <p>Transaction Hash: {data?.hash}</p>}
+          {/* {isSuccess && <p>Transaction Hash: {data?.hash}</p>}
       {writeError && (
         <p className="text-red-500">Error: {writeError?.message}</p>
       )} */}
-    </div>
-    
-    <div className="add_fund_sec">
-        <form className="fund-form">
-          <input type="text" placeholder="Enter Amount in USDT" className="input-field"/>
-          <select name="fund-option" className="select-field">
-            <option value="USDT(BEP20)">USDT (BEP20)</option>
-          </select>
-          <button className="connect-wallet-btn">Connect Wallet</button>
-        </form>
-      </div>
+        </div>
+
+        <div className="add_fund_sec">
+          <form className="fund-form">
+            <input
+              type="text"
+              placeholder="Enter Amount in USDT"
+              className="input-field"
+            />
+            <select name="fund-option" className="select-field">
+              <option value="USDT(BEP20)">USDT (BEP20)</option>
+            </select>
+            <button className="connect-wallet-btn">Connect Wallet</button>
+          </form>
+        </div>
       </MasterLayout>
     </>
-   
   );
 };
 
