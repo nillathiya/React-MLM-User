f8841e66387f1e5faee7b7c645a88c4cdf3c2197
nillathiya <73015219+nillathiya@users.noreply.github.com>
2025-02-19 09:21:41 +0530

    Feat: added add fund logic

--- src/pages/Fund/addFund.jsx
+++ src/pages/Fund/addFund.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  useAccount,
+  useWriteContract,
+  useWaitForTransactionReceipt,
+} from "wagmi";
+import { parseUnits } from "viem";
+import { abi } from "../../ABI/usdtAbi";
+import { toast } from "react-hot-toast";
+
+const USDT_ADDRESS = "0x7B5E2af1a89a1a23D8031077a24A2454D81b3fbd";
 
 const AddFund = () => {
-  return (
-    <div>
-      adfund
-    </div>
-  )
-}
+  const { address } = useAccount();
+  console.log("address: " + address);
+  const [recipient, setRecipient] = useState("");
+  // const [amount, setAmount] = useState("");
 
-export default AddFund
+  const {
+    writeContract,
+    data: hash,
+    status,
+    variables,
+    writeContractAsync,
+  } = useWriteContract();
+
+  const amount = 1 * 10 ** 18;
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    });
+  console.log("data", hash);
+
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">Send USDT</h2>
+      <input
+        type="text"
+        placeholder="Recipient Address"
+        className="border p-2 rounded w-full mb-2"
+        value={recipient}
+        onChange={(e) => setRecipient(e.target.value)}
+      />
+      {/* <input
+        type="text"
+        placeholder="Amount"
+        className="border p-2 rounded w-full mb-2"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      /> */}
+      <button
+        className="bg-blue-500 text-white p-2 rounded"
+        onClick={() =>
+          writeContractAsync({
+            abi,
+            address: USDT_ADDRESS,
+            functionName: "transfer",
+            args: ["0x3Ca63F13dfDC749Cc5565c4Ab9b00657e1b4f379", amount],
+          })
+        }
+        // disabled={isLoading}
+      >
+        {/* {isLoading ? "Sending..." : "Send USDT"} */}
+        send USDT
+      </button>
+      {hash && <div>Transaction Hash: {hash}</div>}
+      {isConfirming && <div>Waiting for confirmation...</div>}
+      {isConfirmed && <div>Transaction confirmed.</div>}
+
+      {/* {isSuccess && <p>Transaction Hash: {data?.hash}</p>}
+      {writeError && (
+        <p className="text-red-500">Error: {writeError?.message}</p>
+      )} */}
+    </div>
+  );
+};
+
+export default AddFund;
