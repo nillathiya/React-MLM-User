9d2ebdb250bc7615acef2c18c1575f390a2de106
Litukumardakua <dakualitukumar147@gmail.com>
2025-02-19 11:01:57 +0530

    changes UI in fund

--- src/pages/Fund/addFund.jsx
+++ src/pages/Fund/addFund.jsx
@@ -1,6 +1,10 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import MasterLayout from "../../masterLayout/MasterLayout";
 import Breadcrumb from "../../components/Breadcrumb";
+import { FaRegCreditCard } from "react-icons/fa";
+import { FaWallet } from "react-icons/fa";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { FaDollarSign } from "react-icons/fa";
 import "./fund.css";
 import {
   useAccount,
@@ -14,10 +18,12 @@ import { toast } from "react-hot-toast";
 const USDT_ADDRESS = "0x7B5E2af1a89a1a23D8031077a24A2454D81b3fbd";
 
 const AddFund = () => {
+  const [selectedToken, setSelectedToken] = useState("USDC");
   const { address } = useAccount();
   console.log("address: " + address);
   const [recipient, setRecipient] = useState("");
   // const [amount, setAmount] = useState("");
+  const [amountInput, setAmountInput] = useState("");
 
   const {
     writeContract,
@@ -78,18 +84,53 @@ const AddFund = () => {
       )} */}
         </div>
 
-        <div className="add_fund_sec">
-          <form className="fund-form">
+        <div className="payment-container">
+          <select className="payment-dropdown">
+            <option>Direct Payment</option>
+            <option>Bank Transfer</option>
+          </select>
+
+          <div className="payment-box">
+            <label>User Name or Wallet Address</label>
             <input
               type="text"
-              placeholder="Enter Amount in USDT"
+              placeholder="Enter username or wallet"
               className="input-field"
             />
-            <select name="fund-option" className="select-field">
-              <option value="USDT(BEP20)">USDT (BEP20)</option>
-            </select>
-            <button className="connect-wallet-btn">Connect Wallet</button>
-          </form>
+
+            <div className="amount-section">
+              <div className="amount-input">
+                <label>Amount</label>
+                <input
+                  type="text"
+                  placeholder="0.00"
+                  className="input-field"
+                  value={amountInput}
+                  onChange={(e) => setAmountInput(e.target.value)}
+                />
+              </div>
+
+              <div className="token-select">
+                <label className="d-flex justify-content-between">
+                  Token <span className="balance">Balance: 0.00</span>
+                </label>
+                <div className="token-dropdown">
+                  <FaDollarSign className="token-icon" />
+                  <select
+                    value={selectedToken}
+                    onChange={(e) => setSelectedToken(e.target.value)}
+                  >
+                    <option value="token">Token Select</option>
+                    <option value="USDT">USDT</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <button className={`submit-btn ${amountInput ? "active-btn" : ""}`}>
+              Enter Detail
+            </button>
+          </div>
         </div>
       </MasterLayout>
     </>
--- src/pages/Fund/addFundArb.jsx
+++ src/pages/Fund/addFundArb.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 import MasterLayout from "../../masterLayout/MasterLayout";
 import Breadcrumb from "../../components/Breadcrumb";
-import './fund.css'
+import "./fund.css";
 
 const AddFundArb = () => {
   return (
     <MasterLayout>
       <Breadcrumb title="Add Fund"></Breadcrumb>
-      <div className="add_fund_sec">
-        <form className="fund-form">
-          <input type="text" placeholder="Enter Amount in ARB" className="input-field"/>
-          <select name="fund-option" className="select-field">
-            <option value="USDT(BEP20)">ARB (BEP20)</option>
-          </select>
-          <button className="connect-wallet-btn">Connect Wallet</button>
-        </form>
+      <div className="payment-container">
+        <div className="payment-box">
+          {/* <label>Enter Amount ARB</label> */}
+          <input
+            type="text"
+            placeholder="Enter Amount ARB"
+            className="input-field"
+          />
+
+          <div className="amount-section">
+            <div className="token-selectarb">
+              <div className="token-dropdownarb">
+                <select>
+                  <option value="token">Token Select</option>
+                  <option value="USDT">USDT</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <button className="submit-btn">Connect Wallet</button>
+        </div>
       </div>
     </MasterLayout>
-  )
-}
+  );
+};
 
-export default AddFundArb
+export default AddFundArb;
