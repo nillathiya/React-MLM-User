0d2813201062044a2310cb9b2c7ee8ff4638a8ca
ridhdhi <ridhdhibhut3@gmail.com>
2025-02-21 10:02:59 +0530

    FIX: Table Responsive

--- src/pages/Genelogy/direct.jsx
+++ src/pages/Genelogy/direct.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import $ from "jquery";
 import "datatables.net-dt/js/dataTables.dataTables.js";
-import { Icon } from "@iconify/react/dist/iconify.js";
 import { Link } from "react-router-dom";
 import MasterLayout from "../../masterLayout/MasterLayout";
 import Skeleton from "../../helper/Skeleton/Skeleton";
@@ -55,74 +54,76 @@ const Direct = () => {
           <h5 className="card-title mb-0">Default Data Tables</h5>
         </div>
         <div className="card-body">
-          <table
-            className="table bordered-table mb-0"
-            id="dataTable"
-            data-page-length={10}
-          >
-            <thead>
-              <tr>
-                <th scope="col">S.L</th>
-                <th scope="col">UserName</th>
-                <th scope="col">Join Date</th>
-                <th scope="col">Name</th>
-                <th scope="col">Mobile</th>
-                <th scope="col">Status</th>
-                <th scope="col">Package</th>
-              </tr>
-            </thead>
-            <tbody>
-              {loading ? (
-                <>
-                  {[...Array(5)].map((_, index) => (
-                    <tr key={index}>
+          <div className="table-container">
+            <table
+              className="table bordered-table mb-0"
+              id="dataTable"
+              data-page-length={10}
+            >
+              <thead>
+                <tr>
+                  <th scope="col">S.L</th>
+                  <th scope="col">UserName</th>
+                  <th scope="col">Join Date</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Mobile</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Package</th>
+                </tr>
+              </thead>
+              <tbody>
+                {loading ? (
+                  <>
+                    {[...Array(5)].map((_, index) => (
+                      <tr key={index}>
+                        <td>
+                          <Skeleton width="50px" height="20px" />
+                        </td>
+                        <td>
+                          <Skeleton width="120px" height="20px" />
+                        </td>
+                        <td>
+                          <Skeleton width="100px" height="20px" />
+                        </td>
+                        <td>
+                          <Skeleton width="150px" height="20px" />
+                        </td>
+                        <td>
+                          <Skeleton width="80px" height="20px" />
+                        </td>
+                        <td>
+                          <Skeleton width="60px" height="20px" />
+                        </td>
+                        <td>
+                          <Skeleton width="90px" height="20px" />
+                        </td>
+                      </tr>
+                    ))}
+                  </>
+                ) : (
+                  users.map((user, index) => (
+                    <tr key={user.id}>
+                      <td>{index + 1}</td>
                       <td>
-                        <Skeleton width="50px" height="20px" />
+                        <Link to="#" className="text-primary-600">
+                          #{user.id}
+                        </Link>
                       </td>
+                      <td>{user.date}</td>
+                      <td>{user.name}</td>
+                      <td>{user.mobile}</td>
                       <td>
-                        <Skeleton width="120px" height="20px" />
+                        <span className="bg-success-focus text-success-main px-24 py-4 rounded-pill fw-medium text-sm">
+                          {user.status}
+                        </span>
                       </td>
-                      <td>
-                        <Skeleton width="100px" height="20px" />
-                      </td>
-                      <td>
-                        <Skeleton width="150px" height="20px" />
-                      </td>
-                      <td>
-                        <Skeleton width="80px" height="20px" />
-                      </td>
-                      <td>
-                        <Skeleton width="60px" height="20px" />
-                      </td>
-                      <td>
-                        <Skeleton width="90px" height="20px" />
-                      </td>
+                      <td>{user.package}</td>
                     </tr>
-                  ))}
-                </>
-              ) : (
-                users.map((user, index) => (
-                  <tr key={user.id}>
-                    <td>{index + 1}</td>
-                    <td>
-                      <Link to="#" className="text-primary-600">
-                        #{user.id}
-                      </Link>
-                    </td>
-                    <td>{user.date}</td>
-                    <td>{user.name}</td>
-                    <td>{user.mobile}</td>
-                    <td>
-                      <span className="bg-success-focus text-success-main px-24 py-4 rounded-pill fw-medium text-sm">
-                        {user.status}
-                      </span>
-                    </td>
-                    <td>{user.package}</td>
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </table>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </MasterLayout>
